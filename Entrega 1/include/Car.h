d74d1ed94793eece08987caedc5045096511fa28
Jonathan Raphael <jonathanraphael74@outlook.com>
2021-06-17 18:57:51 -0300

    entities added

---
+++ Entrega 1/include/Car.h
@@ -0,0 +1,14 @@
+#ifndef __CAR_H__
+#define __CAR_H__
+
+typedef struct Car{
+    char plate[10];
+    char barnd[30];
+    char model[30];
+    int year;
+    int mileage;
+    int quantity;
+    int category;
+}TCar;
+
+#endif
