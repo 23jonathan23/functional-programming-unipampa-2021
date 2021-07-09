fee324311ab9b46e3c193516215668af0fb3ae74
Jonathan Raphael <jonathanraphael74@outlook.com>
2021-07-09 14:53:04 -0300

    sales added(facts)

---
+++ Entrega 2/RealEstate.pl
@@ -0,0 +1,17 @@
+% sale(realEstate, housingDetails, housingPrice, nrCustumer, saller)
+sale(alegrete, 'Casa 3 quartos', 300000, 1010, maria).
+sale(alegrete, 'Apartamento 1 quarto', 1010, maria).
+sale(baitachao, 'Casa 3 quartos', 700000, 2010, paulo).
+sale(ibirapuita, 'Apartamento 1 quarto', 130000, 3010, ana).
+sale(ibirapuita, 'Casa 1 quarto', 220000, 3011, jose).
+sale(alegrete, 'Apartamento 3 quartos', 500000, 1011, maria).
+sale(ibirapuita, 'Casa 2 quartos', 250000, 3012, lucas).
+sale(ibirapuita, 'Apartamento 2 quartos', 140000, 3012, lucas).
+sale(baitachao, 'Apartamento 2 quartos', 250000, 2011, marisa).
+sale(alegrete, 'Apartamento 2 quartos', 300000, 1012, rosa).
+sale(baitachao, 'Casa 2 quartos', 260000, 2012, paulo).
+sale(baitachao, 'Casa 1 quarto', 150000, 2012, paulo).
+sale(alegrete, 'Apartamento 3 quartos', 650000, 1013, miguel).
+sale(alegrete, 'Apartamento 1 quarto', 145000, 1013, miguel).
+sale(alegrete, 'Apartamento 2 quartos', 160000, 1013, miguel).
+
